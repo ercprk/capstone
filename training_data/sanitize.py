5636599746d28cb3d55d09dde4e0b16deb08e16c
ercprk <kyoungdukp@gmail.com>
2021-08-13 10:39:48 -0400

    Updated sanitizing logic

--- training_data/sanitize.py
+++ training_data/sanitize.py
@@ -5,53 +5,135 @@ import nltk
 import re
 import html
 import spacy
+from commonregex import CommonRegex
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
 #nltk.download('words')
 
-def sanitize(text):
-    res = ''
-    words = set(nltk.corpus.words.words())
-    nlp = spacy.load("en_core_web_trf")
-
-    # Remove extra whitespaces
-    sanitized = re.sub(r'\s+', ' ', text)
+def sanitize(text, sanitized_filename):
 
     # Remove web URLs
-    sanitized = re.sub(r"http\S+", "", sanitized)
+    sanitized = re.sub(r"http\S+", "", text)
 
     # Remove HTML tags
     sanitized = re.sub('<[^<]+?>', '', sanitized)
 
+    # Remove HTML characters
+    sanitized = re.sub('&[^ ]+;', '', sanitized)
+
+    # Remove state abbreviations
+    sanitized = re.sub('(?<!\w)(?:,\s+)?(?:A[LKZR]|C[AOT]|DE|FL|GA|HI|I[ADLN]|K[SY]|LA|M[EDAINSOT]|N[EVHJMYCD]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY]),?\s?(?!\w)', '', sanitized)
+
+    fo = open('unique_senders.txt')
+    try:
+        text_ = fo.read()
+    finally:
+        fo.close()
+
+    # Load natural language processor and add custom organizations
+    nlp = spacy.load("en_core_web_lg")
+    ruler = nlp.add_pipe("entity_ruler")
+    unique_senders = set(text_.split('\n'))
+    patterns = list()
+    for unique_sender in unique_senders:
+        pattern = {"label": "ORG", "pattern": [{"LOWER": unique_sender}]}
+        patterns.append(pattern)
+    ruler.add_patterns(patterns)
+
+    lemmatizer = WordNetLemmatizer()
     sentences = nltk.sent_tokenize(sanitized)
-    for sentence in sentences:
-        if is_stop_sentence(sentence):
-            continue
+    counter = 0
+    num_sentences = len(sentences)
+    res = ''
+    print('There are %d sentences' % (num_sentences))
 
-        #tokens = nltk.word_tokenize(sentence)
-        #for token in tokens:
-        #    if token not in words:
+    # Write the result
+    try:
+        sanitized_file = open(sanitized_filename, "w")
+        for sentence in sentences:
+            counter += 1
+            if counter % 1000 == 0 or counter == num_sentences:
+                print('%d sentences sanitized (%.2f%% complete)' % (counter, float(counter / num_sentences) * 100))
 
-        print(sentence)
-        doc = nlp(sentence)
-        for ent in doc.ents:
-            print(ent.text + ' ' + ent.label_)
-            if ent.label_ == 'ORG':
-                sentence.replace(ent.text, 'BUSINESS_NAME')
-        
-        res += sentence + '\n'
+            if is_stop_sentence(sentence):
+                continue
 
-        return sentence
-    
-        #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+            #print('1 ' + sentence)
+            doc = nlp(sentence)
+            for ent in doc.ents:
+                #print('ent: %s ent.label: %s' % (ent.text, ent.label_))
+                if ent.label_ == 'ORG' or ent.label_ == 'NORP':
+                    sentence = sentence.replace(ent.text, 'BUSINESSNAME')
+                elif ent.label_ == 'PRODUCT':
+                    sentence = sentence.replace(ent.text, 'PRODUCTNAME')
+                elif ent.label_ == 'PERSON' and ent.label_ == 'GPE':
+                    sentence = sentence.replace(ent.text, '')
+                    
+            #print('2 ' + sentence)
+            # Remove times, dates, and prices
+            parsed_text = CommonRegex(sentence)
+            for price in parsed_text.prices:
+                sentence = sentence.replace(price, '')
 
-    return res
+            #print('3 ' + sentence)
+            # Remove non-dictionary words
+            words = set(nltk.corpus.words.words())
+            puncts = {'.', '"', "'", ',', '-', '%', '!', '?'}
+            etcs = {'email', 'online'}
+            words = words.union(puncts).union(etcs)
+            tokens = nltk.wordpunct_tokenize(sentence)
+            pos_tags = nltk.pos_tag(tokens)
+            #print(pos_tags)
+
+            for token_idx, token in enumerate(tokens):
+                syntactic_category = get_syntactic_category(pos_tags[token_idx][1])
+                if syntactic_category != None:
+                    lemmatized_token = lemmatizer.lemmatize(token.lower(), syntactic_category)
+                    #print('lemmatized: %s original: %s' % (lemmatized_token, token))
+                    if lemmatized_token.lower() not in words and token != 'BUSINESSNAME' and token != 'PRODUCTNAME' and not token.isnumeric():
+                        sentence = sentence.replace(token, '')
+                else:
+                    if token.lower() not in words and token != 'BUSINESSNAME' and token != 'PRODUCTNAME' and not token.isnumeric():
+                        sentence = sentence.replace(token, '')
+
+            #print('4 ' + sentence)
+
+            # Remove extra whitespaces
+            sentence = re.sub(r'\s+', ' ', sentence)
+            sentence = re.sub(r' , ', ' ', sentence)
+            sentence = re.sub(r' \.', '.', sentence)
+            sentence = re.sub(r' !', '!', sentence)
+            sentence = re.sub(r' \?', '?', sentence)
+            #print('5 ' + sentence)
+
+            sanitized_file.write(sentence + '\n')
+
+    finally:
+        sanitized_file.close()
+
+def get_syntactic_category(pos_tag: str) -> str:
+    if pos_tag.startswith('JJ'):
+        return wordnet.ADJ
+    elif pos_tag.startswith('NN'):
+        return wordnet.NOUN
+    elif pos_tag.startswith('RB'):
+        return wordnet.ADV
+    elif pos_tag.startswith('VB'):
+        return wordnet.VERB
+    else:
+        return None
 
 def is_stop_sentence(sentence):
-    words = set(['Copyright'])
+    words = {'copyright', 'unsubscribe', 'instagram', 'twitter', 'facebook', 'youtube', 'style='}
 
     tokens = nltk.word_tokenize(sentence)
     for token in tokens:
-        if token in words:
+        if token.lower() in words:
             return True
+
+    parsed_text = CommonRegex(sentence)
+    if len(parsed_text.street_addresses) > 0 or len(parsed_text.emails) > 0 or len(parsed_text.phones) > 0 or len(parsed_text.times) > 0 or len(parsed_text.dates) > 0 or len(parsed_text.links) > 0 or len(parsed_text.zip_codes) > 0:
+        return True
 
     return False
 
@@ -59,7 +141,6 @@ def main():
 
     # Filepaths
     original_filename = "unsanitized_data.txt"
-    sanitized_filename = "sanitized_data.txt"
 
     # Read unsanitized, original data
     original_file = open(original_filename)
@@ -69,14 +150,7 @@ def main():
         original_file.close()
 
     # Sanitize
-    text = sanitize(text)
-
-    # Write the result
-    try:
-        sanitized_file = open(sanitized_filename, "w")
-        sanitized_file.write(text)
-    finally:
-        sanitized_file.close()
+    sanitize(text, "sanitized_data.txt")
 
 if __name__ == '__main__':
     main()
